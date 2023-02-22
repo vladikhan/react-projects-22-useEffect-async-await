7dd5de7a323d5155bcdc6bed4f1b98f9b6e6ee92
vladikhan <84037014+vladikhan@users.noreply.github.com>
2023-02-23 05:13:03 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Posts from './compnents/Posts'
+
+function App() {
+  return (
+    <div className="App">
+      <Posts />
+    </div>
+  )
+}
+
+export default App
---
+++ src/compnents/Posts.js
@@ -0,0 +1,49 @@
+import Post from './Post'
+import { useState, useEffect } from 'react'
+
+const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+function Posts() {
+  const [posts, setPosts] = useState([])
+  const [error, setError] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(async () => {
+    try {
+      const res = await fetch(API_URL)
+      const posts = await res.json()
+      setPosts(posts)
+    } catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
+  }, [])
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((res) => res.json())
+  //     .then((posts) => setPosts(posts))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false))
+  // }, [])
+
+  // if (isLoading) {
+  //   return <h1>Loading...</h1>
+  // }
+  if (error) {
+    return <h1>Error: {error}</h1>
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr></hr>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts.map((post) => <Post key={post.id} {...post} />)
+      )}
+    </>
+  )
+}
+
+export default Posts
